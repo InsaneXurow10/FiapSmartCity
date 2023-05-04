53f4efba246f9484f63890394ad1a6d40e15162a
Gabriel Llanir <104911512+InsaneXurow10@users.noreply.github.com>
2023-05-04 10:40:48 -0300

    Add files via upload

---
+++ FIAPSmartCity/Controllers/TipoProdutoController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FIAPSmartCity.Controllers
+{
+    public class TipoProdutoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // ERRADO!!!!!!!!!!!!!!!
+        [HttpPost]
+        public void IActionResultCadastrar(Models.TipoProduto tipoProduto) 
+        {
+            // Validando o campo descrição
+            if (string.IsNullOrEmpty (tipoProduto.DescricaoTipo) )
+            {
+                // Adicionando a mensagem de Erro para descrição em branco
+                ModelState.AddModelError("Descrição", "Descrição obrigatória!");
+            }
+
+            if (ModelState.IsValid)
+            {
+                // Simila que os dados foram gravados.
+                System.Diagnostics.Debug.Print("Descrição " + tipoProduto.DescricaoTipo);
+                System.Diagnostics.Debug.Print("Comercializado: " + tipoProduto.Comercializado);
+                System.Diagnostics.Debug.Print("Gravando o Tipo de Produto");
+
+                // return RedirectToAction("Index", "TipoProduto");
+                // Encontrou um erro no preenchimento do campo descrição
+            } else
+            {
+                // retorna para tela do formulário
+                // return View(tipoProduto);
+            }
+        }
+    }
+}
---
+++ FIAPSmartCity/Models/Produto.cs
@@ -0,0 +1,14 @@
+﻿namespace FIAPSmartCity.Models
+{
+    public class Produto
+    {
+        public int IdProduto { get; set; }
+        public String NomeProduto { get; set; }
+        public String Caracteristicas { get; set; }
+        public double PrecoMedio { get; set; }
+        public String LogoTipó { get; set; } 
+        public bool Ativo { get; set; }
+
+        public TipoProduto Tipo { get; set; }
+    }
+}
